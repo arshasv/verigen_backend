4cc4a6332db73dd0615fd8ac5a9b245ea267ddd2
sudheesh-v-r <sudheesh.vr.1805@gmail.com>
2024-11-27 14:18:26 +0530

    add functionality to upload file by authorised user

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
-from routes.rout import router
+from fastapi.middleware.cors import CORSMiddleware
+from routes.route import router
+from routes.blob_route import blob_routes
 
+# Create FastAPI application
 app = FastAPI()
 
+# CORS Configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",  
+        "http://localhost:3001",  
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
+
 app.include_router(router)
+app.include_router(blob_routes, prefix="/storage/blob")
