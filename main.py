e57d5c7cc48e16d2bf2a3631aa787abbb6fe67bd
sudheesh-v-r <sudheesh.vr.1805@gmail.com>
2024-11-27 15:21:43 +0530

    rectified few errors on the code

--- main.py
+++ main.py
@@ -19,19 +19,5 @@ app.add_middleware(
     allow_headers=["*"],  
 )
 
-
-# CORS Configuration
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",  
-        "http://localhost:3001",  
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
-)
-
-
 app.include_router(router)
 app.include_router(blob_routes, prefix="/storage/blob")
